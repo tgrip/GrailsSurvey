6a5f64ec4d866b6aa70e30d4b18f0a12c662cc01
Theo Grip <theo.grip@googlemail.com>
2012-12-31 15:11:54 -0600

    add admin user

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,9 @@
+import com.tgrip.survey.Person
+
 class BootStrap {
 
     def init = { servletContext ->
+        new Person(username: 'admin', password: '123123', enabled: true).save()
     }
     def destroy = {
     }
